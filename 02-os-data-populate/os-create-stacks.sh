529e073f2767d6b730e89b94d7794e068f44070b
Marko Kosmerl <marko.kosmerl@gmail.com>
2017-11-24 11:38:53 +0100

    Simplifying POC heat templates

--- 02-os-data-populate/os-create-stacks.sh
+++ 02-os-data-populate/os-create-stacks.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
 source ~/keystonerc_admin
-
-set -x
 script_dir=$(dirname "$0")
 
-heat stack-create -f $script_dir/heat-lbaas-tcp.yaml test-tcp
-heat stack-create -f $script_dir/heat-lbaas-http-1.yaml test-http-1
-heat stack-create -f $script_dir/heat-lbaas-http-2.yaml test-http-2
+set -x
 
+NETWORK=private
+NETWORK_PUB=public
+KEYNAME=test-key
+
+neutron net-update --shared=True $NETWORK
+
+test -f ~/.ssh/id_rsa || ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+nova keypair-add --pub-key ~/.ssh/id_rsa.pub $KEYNAME
+
+PROJ_ID=$(openstack project show -f value -c id admin)
+openstack quota set --volumes -1 --cores -1 --ram -1 --secgroups -1 --floating-ips -1 --instances -1 $PROJ_ID
+
+for name in allow_everything
+do
+  neutron security-group-create $name
+  neutron security-group-rule-create $name --protocol tcp
+  neutron security-group-rule-create $name --protocol udp
+  neutron security-group-rule-create $name --protocol icmp
+done
+
+heat stack-create -f $script_dir/heat-lbaas-http.yaml test-http-1
+heat stack-create -f $script_dir/heat-lbaas-http.yaml test-http-2
